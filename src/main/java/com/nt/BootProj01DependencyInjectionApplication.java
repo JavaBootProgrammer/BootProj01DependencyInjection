fcaad3977a2ea39a4dc756c61f9dc8118732c239
anand8533 <anand8533@gmail.com>
2024-05-21 14:04:33 -0400

    added UML

--- src/main/java/com/nt/BootProj01DependencyInjectionApplication.java
+++ src/main/java/com/nt/BootProj01DependencyInjectionApplication.java
@@ -14,25 +14,24 @@ import com.nt.sbeans.SeasonFinder;
 @SpringBootApplication
 
 public class BootProj01DependencyInjectionApplication {
-	
-	
-	@Bean(name="dt")
-	public  LocalDate  createDate() {
+
+	@Bean(name = "dt")
+	public LocalDate createDate() {
 		System.out.println("BootProj01DependencyInjectionApplication.createDate()");
 		return LocalDate.now();
 	}
 
 	public static void main(String[] args) {
-		//get IOC cointainer
-		   ApplicationContext ctx=SpringApplication.run(BootProj01DependencyInjectionApplication.class, args);
-		    //get Spring bean class obj ref (Target)
-		   SeasonFinder  finder=ctx.getBean("sf",SeasonFinder.class);
-		   //invoke the b.method
-		   String resultMsg=finder.findSeason();
-		   System.out.println("output is "+resultMsg);
-		   
-		   //close container
-		   ((ConfigurableApplicationContext) ctx).close();
+		// get IOC cointainer
+		ApplicationContext ctx = SpringApplication.run(BootProj01DependencyInjectionApplication.class, args);
+		// get Spring bean class obj ref (Target)
+		SeasonFinder finder = ctx.getBean("sf", SeasonFinder.class);
+		// invoke the b.method
+		String resultMsg = finder.findSeason();
+		System.out.println("output is " + resultMsg);
+
+		// close container
+		((ConfigurableApplicationContext) ctx).close();
 	}
 
 }
--- src/main/java/com/nt/sbeans/SeasonFinder.java
+++ src/main/java/com/nt/sbeans/SeasonFinder.java
@@ -9,21 +9,21 @@ import org.springframework.stereotype.Component;
 @Component("sf")
 public class SeasonFinder {
 	@Autowired
-	private  LocalDate  date;
-	
+	private LocalDate date;
+
 	public SeasonFinder() {
 		System.out.println("SeasonFinder:: 0-param constructor");
 	}
-	
+
 	// b.method
-	public  String findSeason() {
+	public String findSeason() {
 		System.out.println("SeasonFinder.findSeason()");
-		//get current month value
-		int monthValue=date.getMonthValue();
-		//write b.logic
-		if(monthValue>=3 && monthValue<=6)
+		// get current month value
+		int monthValue = date.getMonthValue();
+		// write b.logic
+		if (monthValue >= 3 && monthValue <= 6)
 			return "Summer Season";
-		else if(monthValue>=7 && monthValue<=10 )
+		else if (monthValue >= 7 && monthValue <= 10)
 			return "Rainy Season";
 		else
 			return "Winter Season";
